16b70eca4cb732561527c50a003f6aa93d240806
Isaac Aguirre <isaac.aguirre@du.edu>
2021-09-19 13:27:29 -0600

    added routes

--- src/App.js
+++ src/App.js
@@ -15,13 +15,15 @@ class App extends Component {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <Nav.Link href="/home">Home</Nav.Link>
+                            <Nav.Link href="/aboutme">About me</Nav.Link>
+                            <Nav.Link href="/resume">Resume</Nav.Link>
+                            <Nav.Link href="/Contact me">Contact</Nav.Link>
+                            <NavDropdown title="Projects" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/projects">All Projects</NavDropdown.Item>
+                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
--- src/components/main.js
+++ src/components/main.js
@@ -2,10 +2,19 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Home from "./home";
+import AboutMe from "./aboutme";
+import Contact from "./contactme";
+import Projects from "./projects";
+import Resume from "./resume";
 
 const Main = () => (
     <Switch>
         <Route exact path = "/" component = {Home} />
+        <Route path = "/home" component = {Home} />
+        <Route path = "/aboutme" component = {AboutMe} />
+        <Route path = "/contactme" component = {Contact} />
+        <Route path = "/projects" component = {Projects} />
+        <Route path = "/resume" component = {Resume} />
     </Switch>
     )
 
