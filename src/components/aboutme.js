f510bdcc393bc3d04846163edad30ce75ae372c5
Isaac Aguirre <isaac.aguirre@du.edu>
2021-09-20 18:24:15 -0600

    setting about me

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import { Avatar } from '@mui/material';
+import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 
 class AboutMe extends Component {
     render() {
         return(
             <div>
                 <h1>About Me</h1>
+                <MDBContainer>
+                    <div className="mx-auto">
+                <div style={{backgroundColor: "lightblue"}}>
+                <Avatar
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                    sx={{ width: 200, height: 200 }}
+                />
+                    <h1>Isaac Aguirre</h1>
+                </div>
+
+                        Centered element
+                    </div>
+                </MDBContainer>
             </div>
         )
     }
