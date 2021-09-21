92fc16599923f9e33af94a2275d86983be8a3dcd
Isaac Aguirre <isaac.aguirre@du.edu>
2021-09-21 14:07:04 -0600

    Temp home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import AboutMe from "./aboutme";
+import Contact from "./contactme";
+import Projects from "./projects";
+
 
 class Home extends Component {
     render() {
         return(
             <div>
                 <h1>Home</h1>
+                <div style={{BackgroundColor: "green"}}>
+
+                <AboutMe/>
+
+                </div>
+                <Projects/>
+                <Contact/>
             </div>
         )
     }
