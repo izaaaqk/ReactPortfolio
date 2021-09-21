84c0b264ff3c03f6d2198f3fc45ad8f6ce266b61
Isaac Aguirre <isaac.aguirre@du.edu>
2021-09-21 01:58:26 -0600

    Added aboutme text and icons

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Avatar } from '@mui/material';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBIcon } from 'mdbreact';
 import pic from '../pic.jpg';
+
 
 
 class AboutMe extends Component {
@@ -9,20 +10,32 @@ class AboutMe extends Component {
         return(
             <div>
                 <h1>About Me</h1>
-                <MDBContainer>
-                    <div className="mx-auto">
-                <div style={{backgroundColor: "lightblue"}}>
+                <MDBContainer style={{ marginLeft: "25%", marginRight: "25%"}}>
+                    <div>
+
                 <Avatar
-                    alt="Remy Sharp"
+                    alt="Isaac Aguirre"
                     src={pic}
                     sx={{ width: 200, height: 200 }}
                 />
                     <h1>Isaac Aguirre</h1>
-                </div>
 
-                        Centered element
                     </div>
                 </MDBContainer>
+                <MDBContainer>
+                    <h4 style={{marginBottom: "2%"}}>I'm a Full-stack software developer with experience in:</h4>
+
+                    <h3 style={{listStyleType: "none", marginLeft: "5%"}}>
+
+                    <li style={{marginBottom: "2%"}}><MDBIcon fab icon="html5" />  HTML</li>
+                    <li style={{marginBottom: "2%"}}><MDBIcon fab icon="css3-alt" />  CSS</li>
+                    <li style={{marginBottom: "2%"}}><MDBIcon fab icon="js" />  JavaScript</li>
+                    <li style={{marginBottom: "2%"}}><MDBIcon fab icon="node" />  NodeJS</li>
+                    <li style={{marginBottom: "2%"}}><MDBIcon fab icon="react" />  React</li>
+                    </h3>
+
+                </MDBContainer>
+
             </div>
         )
     }
