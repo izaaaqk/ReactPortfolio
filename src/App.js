7dbaac579be5f2ff3b9c50d6eb598e8831b19e9b
Isaac Aguirre <isaac.aguirre@du.edu>
2021-10-25 19:22:22 -0600

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
             <Navbar bg="info" expand="lg">
 
                 <Container>
-                    <Navbar.Brand href="#home">Isaac Aguirre</Navbar.Brand>
+                    <Navbar.Brand href="home">Isaac Aguirre</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
